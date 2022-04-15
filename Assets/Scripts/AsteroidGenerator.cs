a70ba4a20bfe18a36186355dd97ecf9e2ef1756d
César Zárate <cesarjzo@outlook.com>
2022-04-15 13:40:54 -0500

    Add asteroid list and update sprites properties

--- Assets/Scripts/AsteroidGenerator.cs
+++ Assets/Scripts/AsteroidGenerator.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 
 public class AsteroidGenerator : MonoBehaviour
 {
+    public List<GameObject> asteroids;
     [SerializeField] float _waitingTime = 1;
     [SerializeField] GameObject _asteroid;
     [SerializeField] Boundary _boundary;
@@ -12,6 +14,7 @@ public class AsteroidGenerator : MonoBehaviour
 
     private void Awake()
     {
+        asteroids = new List<GameObject>();
         _generationPoint = new Vector3(0, _boundary.MaxY, 0);
         waitForSeconds = new WaitForSeconds(_waitingTime);
     }
@@ -22,9 +25,8 @@ public class AsteroidGenerator : MonoBehaviour
     IEnumerator GenerateAsteroid()
     {
         _generationPoint.x = Random.Range(_boundary.MinX, _boundary.MaxX);
-        Instantiate(_asteroid, _generationPoint, Quaternion.identity);
+        asteroids.Add(Instantiate(_asteroid, _generationPoint, Quaternion.identity));
         yield return waitForSeconds;
         StartCoroutine(GenerateAsteroid());
-
     }
 }
